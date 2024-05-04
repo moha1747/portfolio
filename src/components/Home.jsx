9bdd3306199d0731f1cb3e43463d93525b73d5c5
moha1747 <mohamud.abbaas14@gmail.com>
2024-05-04 10:59:30 -0500

    ui fixes for homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,75 +3,79 @@ import Typewriter from "typewriter-effect";
 import { Bio } from "../data/Bio";
 import hero from "../images/hero.png";
 import { ThemeProviderWrapper } from "../utils/ThemeProviderWrapper";
+import Skills from "./Skills";
 
 const Home = () => {
   return (
-    <ThemeProviderWrapper>
-      <Container maxWidth="lg" sx={{ display: "flex", flexDirection: "row" }}>
-        <Box
-          component="section"
-          height={400}
-          width={500}
-          sx={{
-            mt: 12,
-            // border: "2px solid red",
-            display: "flex",
-            flexDirection: "column",
-            gap: 2,
-          }}
-        >
-          <Typography variant="h1">
-            Hi, I am <br /> Abbaas Mohamud
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
-            <Typography variant="h4">I am a </Typography>
-            <Typography variant="h4" color="primary">
-              <Typewriter
-                options={{
-                  strings: Bio.roles,
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
-            </Typography>
-          </Box>
-          <Typography variant="content" color="secondary">
-            {" "}
-            {Bio.description}
-          </Typography>
-          <Button
-            variant="contained"
+    <>
+      <ThemeProviderWrapper mode>
+        <Container maxWidth="lg" sx={{ display: "flex", flexDirection: "row" }}>
+          <Box
+            component="section"
+            height={400}
+            width={500}
             sx={{
-              textDecoration: "none",
-              width: "95%",
-              maxWidth: 300,
-              height: 200,
-              textAlign: "center",
-              padding: "12px 0px",
-              background:
-                "linear-gradient(225deg, rgb(132, 0, 255) 0%, rgb(230, 0, 255) 100%)",
-              //   boxShadow:
-              //     "20px 20px 60px rgb(31, 38, 52), -20px -20px 60px rgb(31, 38, 52)",
-              borderRadius: 50,
-              fontWeight: 600,
-              fontSize: 20,
-              color: "inherit",
-              mt: 3,
+              mt: 12,
+              // border: "2px solid red",
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
             }}
           >
-            Check Resume
-          </Button>
-        </Box>
-        <Box sx={{ ml: 24, mt: 6}}>
-          <img
-            src={hero}
-            alt=""
-            height={500}
-            style={{ background: "transparent" }}
-          />
-        </Box>
-      </Container>
-    </ThemeProviderWrapper>
+            <Typography variant="h1">
+              Hi, I am <br /> Abbaas Mohamud
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", gap: 1 }}>
+              <Typography variant="h4">I am a </Typography>
+              <Typography variant="h4" color="primary">
+                <Typewriter
+                  options={{
+                    strings: Bio.roles,
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </Typography>
+            </Box>
+            <Typography variant="content" color="secondary">
+              {" "}
+              {Bio.description}
+            </Typography>
+            <Button
+              variant="contained"
+              sx={{
+                textDecoration: "none",
+                width: "95%",
+                maxWidth: 300,
+                height: 200,
+                textAlign: "center",
+                padding: "12px 0px",
+                background:
+                  "linear-gradient(225deg, rgb(132, 0, 255) 0%, rgb(230, 0, 255) 100%)",
+                //   boxShadow:
+                //     "20px 20px 60px rgb(31, 38, 52), -20px -20px 60px rgb(31, 38, 52)",
+                borderRadius: 50,
+                fontWeight: 600,
+                fontSize: 20,
+                color: "inherit",
+                mt: 3,
+              }}
+            >
+              Check Resume
+            </Button>
+          </Box>
+          <Box sx={{ ml: 24, mt: 6 }}>
+            <img
+              src={hero}
+              alt=""
+              height={500}
+              style={{ background: "transparent" }}
+            />
+          </Box>
+        </Container>
+      </ThemeProviderWrapper>
+    <Skills />
+    </>
   );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,50 @@
 // import { Bio } from "../data/Bio"
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography, Card } from "@mui/material";
+import { ThemeProviderWrapper } from "../utils/ThemeProviderWrapper";
+
 const Skills = () => {
   return (
-    <Container
-      maxWidth="lg"
-      sx={{ height: "100vh", border: "2px solid green" }}
-    >
-      <Box height={"100vh"} mt={8}>
-        <Typography variant="h1" sx={{ letterSpacing: 2, mb: 3, textAlign: "center"}} color="inherit">Skills</Typography>
-        <Typography variant="h6"sx={{fontWeight: "bold", textAlign: "center"}} color="secondary">Here are the skills I have learned through Projects, Coursework and Experience</Typography>
+    <ThemeProviderWrapper>
+      <Container
+        maxWidth="lg"
+        sx={{ height: "100vh", border: "2px solid green" }}
+      >
+        <Box height={"100vh"} mt={8}>
+          <Typography
+            variant="h1"
+            sx={{ letterSpacing: 2, mb: 3, textAlign: "center" }}
+            color="inherit"
+          >
+            Skills
+          </Typography>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: "bold", textAlign: "center" }}
+            color="secondary"
+          >
+            Here are the skills I have learned through Projects, Coursework and
+            Experience
+          </Typography>
 
-      </Box>
-    </Container>
+          <Card
+            variant="outlined"
+            sx={{
+              height: 400,
+              width: 500,
+              boxShadow: 3,
+              backgroundColor: "inherit",
+            }}
+          >
+            <Typography variant="h3" color="primary">
+              Frontend
+            </Typography>
+          </Card>
+        </Box>
+      </Container>
+    </ThemeProviderWrapper>
   );
+      
+
 }
 
 export default Skills
