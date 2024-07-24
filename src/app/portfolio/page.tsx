b54ea473076ad32e0446fb339a32cb5fdca38b9e
moha1747 <mohamud.abbaas14@gmail.com>
2024-07-24 10:04:46 -0500

    Vercel Deployment commit

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -15,10 +15,6 @@ export default function Portfolio() {
     <Container>
       <section className="mb-12">
         <h2 className="mb-4 text-5xl font-bold">Portfolio</h2>
-        <p>
-          Here are some of my works. I have worked on various projects, from a
-          student productivity platform to a Algorithm visualizer game. I have programming experience in JavaScript, Python, Java, SQL. I have experience with frameworks such as React, Spring Boot and MySQL. 
-        </p>
       </section>
 
       <PortfolioList portfolioData={portfolioData} />
